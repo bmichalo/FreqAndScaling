8083692e0b63a423316f83b76c7a0aeb1b43f17a
root <root@perf8.perf.lab.eng.bos.redhat.com>
2022-04-19 08:31:55 -0400

    cpufreq and intel_pstate sysfs file structure may be different for different h/w platforms.  Changed sysfs parsing to be more robust across all achitectures

--- tools/gather_system_power_info.sh
+++ tools/gather_system_power_info.sh
@@ -58,18 +58,11 @@ echo "========================"
 
 for i in 3 51 5 53 7 55 9 57;
 do
-	echo "cpu" $i "/sys/devices/system/cpu/cpufreq/policy$i/cpuinfo_min_freq ............... `cat /sys/devices/system/cpu/cpufreq/policy$i/cpuinfo_min_freq`" 
-	echo "cpu" $i "/sys/devices/system/cpu/cpufreq/policy$i/cpuinfo_max_freq ............... `cat /sys/devices/system/cpu/cpufreq/policy$i/cpuinfo_max_freq`" 
-	echo "cpu" $i "/sys/devices/system/cpu/cpufreq/policy$i/scaling_setspeed ............... `cat /sys/devices/system/cpu/cpufreq/policy$i/scaling_setspeed`" 
-	echo "cpu" $i "/sys/devices/system/cpu/cpufreq/policy$i/scaling_min_freq ............... `cat /sys/devices/system/cpu/cpufreq/policy$i/scaling_min_freq`" 
-	echo "cpu" $i "/sys/devices/system/cpu/cpufreq/policy$i/scaling_max_freq ............... `cat /sys/devices/system/cpu/cpufreq/policy$i/scaling_max_freq`" 
-	echo "cpu" $i "/sys/devices/system/cpu/cpufreq/policy$i/scaling_governor ............... `cat /sys/devices/system/cpu/cpufreq/policy$i/scaling_governor`" 
-	echo "cpu" $i "/sys/devices/system/cpu/cpufreq/policy$i/scaling_driver ................. `cat /sys/devices/system/cpu/cpufreq/policy$i/scaling_driver`" 
-	echo "cpu" $i "/sys/devices/system/cpu/cpufreq/policy$i/scaling_cur_freq ............... `cat /sys/devices/system/cpu/cpufreq/policy$i/scaling_cur_freq`" 
-	echo "cpu" $i "/sys/devices/system/cpu/cpufreq/policy$i/scaling_available_governors .... `cat /sys/devices/system/cpu/cpufreq/policy$i/scaling_available_governors`" 
-	echo "cpu" $i "/sys/devices/system/cpu/cpufreq/policy$i/related_cpus ................... `cat /sys/devices/system/cpu/cpufreq/policy$i/related_cpus`" 
-	echo "cpu" $i "/sys/devices/system/cpu/cpufreq/policy$i/cpuinfo_transition_latency ..... `cat /sys/devices/system/cpu/cpufreq/policy$i/cpuinfo_transition_latency`" 
-	echo "cpu" $i "/sys/devices/system/cpu/cpufreq/policy$i/affected_cpus .................. `cat /sys/devices/system/cpu/cpufreq/policy$i/affected_cpus`" 
+	tree /sys/devices/system/cpu/cpufreq/policy$i
+
+	for file in /sys/devices/system/cpu/cpufreq/policy$i/*; do
+		echo "$file .... `cat $file`"
+	done
 	echo ""
 done
 
@@ -101,15 +94,25 @@ echo "intel_pstate driver information:"
 echo "================================"
 echo ""
 
+
+for i in 3 51 5 53 7 55 9 57;
+do
+	tree /sys/devices/system/cpu/cpufreq/policy$i
+
+	for file in /sys/devices/system/cpu/cpufreq/policy$i/*; do
+		echo "$file .... `cat $file`"
+	done
+	echo ""
+done
+
+
 if [ -d "/sys/devices/system/cpu/intel_pstate" ]; then
 
-	echo "/sys/devices/system/cpu/intel_pstate/turbo_pct ....... `cat /sys/devices/system/cpu/intel_pstate/turbo_pct`"
-	echo "/sys/devices/system/cpu/intel_pstate/status .......... `cat /sys/devices/system/cpu/intel_pstate/status`"
-	echo "/sys/devices/system/cpu/intel_pstate/num_pstates ..... `cat /sys/devices/system/cpu/intel_pstate/num_pstates`"
-	echo "/sys/devices/system/cpu/intel_pstate/min_perf_pct .... `cat /sys/devices/system/cpu/intel_pstate/min_perf_pct`"
-	echo "/sys/devices/system/cpu/intel_pstate/max_perf_pct .... `cat /sys/devices/system/cpu/intel_pstate/max_perf_pct`"
-	echo "/sys/devices/system/cpu/intel_pstate/no_turbo ........ `cat /sys/devices/system/cpu/intel_pstate/no_turbo`"
+	tree /sys/devices/system/cpu/intel_pstate
 
+	for file in /sys/devices/system/cpu/intel_pstate/*; do
+		echo "$file .... `cat $file`"
+	done
 else
 	echo "Directory /sys/devices/system/cpu/intel_pstate does not exist"
 fi
@@ -120,15 +123,16 @@ echo ""
 echo "Idle states:"
 echo "============"
 
-echo "/sys/module/intel_idle/parameters/use_acpi ...... `cat /sys/module/intel_idle/parameters/use_acpi`"
-echo "/sys/module/intel_idle/parameters/states_off .... `cat /sys/module/intel_idle/parameters/states_off`"
-echo "/sys/module/intel_idle/parameters/no_acpi ....... `cat /sys/module/intel_idle/parameters/no_acpi`"
-echo "/sys/module/intel_idle/parameters/max_cstate .... `cat /sys/module/intel_idle/parameters/max_cstate`"
+tree /sys/module/intel_idle/parameters
+
+for file in /sys/module/intel_idle/parameters/*; do
+	echo "$file .... `cat $file`"
+done
 
 echo ""
-echo ""
+echo "Gathering turbostat data for 60 seconds..."
 
-timeout 10s turbostat -c 3,51,5,53,7,55,9,57 -i 3 --out turbostat.txt
+timeout 60s turbostat -c 3,51,5,53,7,55,9,57 -i 3 --out turbostat.txt
 
 echo ""
 echo ""
