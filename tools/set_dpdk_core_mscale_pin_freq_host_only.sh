ba095412698b77c45b6d9206e97ea9ca0c447834
root <root@perf8.perf.lab.eng.bos.redhat.com>
2022-05-11 11:51:03 -0400

    Adding script to manipulate OVS host only freq scaling

---
+++ tools/set_dpdk_core_mscale_pin_freq_host_only.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+for i in 3 51 5 53;
+do
+	echo $1 > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_min_freq
+	echo $1 > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_max_freq
+	echo "cpu" $i "scaling_min_freq = `cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_min_freq`" 
+	echo "cpu" $i "scaling_max_freq = `cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_max_freq`" 
+done
+
+
+
+
+
+
